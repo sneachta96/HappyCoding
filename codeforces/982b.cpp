0872844adf1557b2abc9522cafe597f3f41f16bd
Jasmeet Kaur <mann.jazz96@gmail.com>
2018-05-18 09:55:47 +0530

    Codeforces
    Stack

---
+++ codeforces/982b.cpp
@@ -0,0 +1,46 @@
+//this is a very nice stack implementation problem
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  vector < pair<int,int> > v;
+  for(int i = 1; i <= n;i++){
+    int x;
+    cin>>x;
+    v.push_back(make_pair(x,i));
+  }
+  sort(v.begin(),v.end());
+  int count[n+1] = {0};
+  string s;
+  cin>>s;
+  int currentI = 0;
+  int currentE = 0;
+  int arr[2*n] ;
+  stack <int> st;
+  int k = 0;
+  for(int i = 0; i < 2*n;i++){
+    if(s[i] == '0'){
+      st.push(v[k].second);
+      arr[i] = v[k].second;
+      k+=1;
+
+    }
+    else{
+      int y = st.top();
+      st.pop();
+
+      arr[i] = y;
+    }
+  }
+  for(int i = 0; i< 2*n;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+}
+
